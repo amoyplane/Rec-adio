5ce7bf189bd24c3b20c1ad508f6a47dc52583fd1
李玮祺 <2016011291@secoder.net>
2020-04-05 15:51:30 +0800

    conf file

--- AutoInit.py
+++ AutoInit.py
@@ -1,7 +1,7 @@
 import os
 
 if __name__ == "__main__":
-    os.system("sudo pipenv install")
+    os.system("sudo /home/shazhubusha/.local/bin/pipenv install")
     os.system("sudo mv ./rec_adio.service /etc/systemd/system/")
     os.system("sudo systemctl daemon-reload")
     os.system("sudo systemctl enable rec_adio.service")
